c313d33e1e5b0ec31edfe296464a66e42bd0bbef
nayan-bansal <nayan.7991@gmail.com>
2020-10-15 11:35:36 +0530

    Added: Junit Test To remove word Phrase

--- MyHashMapTest.java
+++ MyHashMapTest.java
@@ -39,4 +39,21 @@ public class MyHashMapTest {
 		System.out.println(myLinkedHashMap);
 		Assert.assertEquals(3, frequency);
 	}
+@Test
+	public void removeWordPhraseTest() {
+		String sentence = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+		MyLinkedHashMap<String, Integer> myLinkedHashMap = new MyLinkedHashMap<String, Integer>();
+		String[] words = sentence.toLowerCase().split(" ");
+		for (String word : words) {
+			Integer value = myLinkedHashMap.get(word);
+			if (value == null)
+				value = 1;
+			else
+				value = value + 1;
+			myLinkedHashMap.add(word, value);
+		}
+		MyMapNode<String, Integer> word = myLinkedHashMap.remove("avoidable");
+		System.out.println(myLinkedHashMap);
+		Assert.assertEquals(word,null);
+	}
 }
