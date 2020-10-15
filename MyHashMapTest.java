112ea887824bd3a03054f380108f8ee301daf7ef
nayan-bansal <nayan.7991@gmail.com>
2020-10-15 11:02:55 +0530

    Added: Junit Test To find frequency of a word in a given Sentence

---
+++ MyHashMapTest.java
@@ -0,0 +1,24 @@
+package Hash_Map;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyHashMapTest {
+	@Test
+	public void wordFrequencySentenceTest() {
+		String sentence = "To be or not to be";
+		MyHashMap<String, Integer> myHashMap = new MyHashMap<String, Integer>();
+		String[] words = sentence.toLowerCase().split(" ");
+		
+		for (String word : words) {
+			Integer value = myHashMap.get(word);
+			if (value == null)
+				value = 1;
+			else
+				value = value + 1;
+			myHashMap.add(word, value);
+		}
+		int frequency = myHashMap.get("to");
+		Assert.assertEquals(2, frequency);
+	}
+}
